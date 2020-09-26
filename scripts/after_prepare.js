0f7b1ae3493e3901cd15f89687be4f5b908e793d
Paulo Santana <paulosantana@MacBook-Air-de-Paulo.local>
2020-09-26 18:49:13 -0300

    Fix to add APP_ID in the browser platform

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -12,11 +12,26 @@ var getPreferenceValue = function(config, name) {
     }
 }
 
-if(process.argv.join("|").indexOf("APP_ID=") > -1) {
-	var APP_ID = process.argv.join("|").match(/APP_ID=(.*?)(\||$)/)[1]
+var getPreferenceValueFromPackageJson = function (config, name) {
+    var value = config.match(new RegExp('"' + name + '":\\s"(.*?)"', "i"));
+    if (value && value[1]) {
+        return value[1]
+    } else {
+        return null
+    }
+};
+
+var APP_ID = '';
+
+if (process.argv.join("|").indexOf("APP_ID=") > -1) {
+    APP_ID = process.argv.join("|").match(/APP_ID=(.*?)(\||$)/)[1]
 } else {
-	var config = fs.readFileSync("config.xml").toString()
-	var APP_ID = getPreferenceValue(config, "APP_ID")
+    var config = fs.readFileSync("config.xml").toString()
+    APP_ID = getPreferenceValue(config, "APP_ID");
+    if (!APP_ID) {
+        var packageJson = fs.readFileSync("package.json").toString();
+        APP_ID = getPreferenceValueFromPackageJson(packageJson, "APP_ID");
+    }
 }
 
 var files = [
